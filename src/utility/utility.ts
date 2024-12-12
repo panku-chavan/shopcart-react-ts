a8fbb6d0e9e5b4ea6f69dd07af4119cbe73d778a
panku-chavan <pankajchavan963@gmail.com>
2024-12-12 17:34:23 +0530

    first commit

--- src/utility/utility.ts
+++
@@ -1,8 +0,0 @@
-import { NextRouter } from "next/router";
-
-export const navigateTo = (router: NextRouter, path: string): void => {
-  if (!router) {
-    throw new Error("Router is not available");
-  }
-  router.push(path);
-};
